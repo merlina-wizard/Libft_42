a8c54ff76a62a29d34243860118511f8d3f8d3ac
Martina Merlini <156202171+merlina-wizard@users.noreply.github.com>
2024-01-11 21:12:27 +0100

    secondo

---
+++ libft/ft_*/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamerlin <mamerlin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 13:56:24 by mamerlin          #+#    #+#             */
+/*   Updated: 2024/01/11 10:50:04 by mamerlin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *restrict dest, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (src[count])
+		count++;
+	if (dstsize < 1)
+		return (count);
+	while (src[i] && i < dstsize - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (count);
+}
