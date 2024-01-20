7f03a0f5554f6cfd3db5fde6d35e1595b777dbc5
Martina Merlini <156202171+merlina-wizard@users.noreply.github.com>
2024-01-20 21:56:38 +0100

    Delete bonus directory

--- bonus/ft_lstdelone.c
+++
@@ -1,7 +0,0 @@
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-	if (!lst || !del)
-		return ;
-	del((void *)lst->content);
-	free(lst);
-}
--- bonus/ft_lstlast.c
+++
@@ -1,9 +0,0 @@
-t_list *ft_lstlast(t_list *lst)
-{
-
-	if (!lst)
-		return (0);
-	while (lst->next != NULL)
-		lst->next = lst;
-	return (lst);
-}
